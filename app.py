e53012c88ef94b49aae6e1c16d2213d0c8a0655e
AliTiflen <alitiflen03@gmail.com>
2023-04-26 12:45:33 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+#pip install Flask
+#pip freeze
+from flask import Flask, render_template
+#from flask_ngrok import run_with_ngrok
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def halamanCV():
+    return render_template("SA2_Deployment.html")
+
+
+if __name__ == "__main__":
+    #run_with_ngrok(app)
+    app.run()
